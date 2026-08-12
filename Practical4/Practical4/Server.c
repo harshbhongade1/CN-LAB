864582872d96d5ac95a3e8285860781fe8c2c8ca
harshbhongade1 <harsh.bhongade.batch2024@sitnagpur.siu.edu.in>
2026-08-12 15:58:37 +0530

    Add files via upload

--- Practical4/Practical4/Server.c
+++ Practical4/Practical4/Server.c
@@ -1,7 +1,7 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include <unistd.h>
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<unistd.h>
 #include<netinet/in.h>
 
 #define PORT 10080
@@ -9,23 +9,21 @@
 
 int main() {
     int sockfd;
-    struct sockaddr_in server;
+    struct sockaddr_in server, client;
     char buffer[BUF_SIZE];
-    socklen_t len = sizeof(server);
+    socklen_t len = sizeof(client);
     sockfd = socket(AF_INET, SOCK_DGRAM, 0);
     server.sin_family = AF_INET;
     server.sin_port = htons(PORT);
     server.sin_addr.s_addr = INADDR_ANY;
-
+    bind(sockfd, (struct sockaddr*)&server, sizeof(server));
+    printf("UDP Echo Server is running on port %d...\n", PORT);
     while(1) {
-        printf("Enter message:");
-        fgets(buffer, BUF_SIZE, stdin);
-        sendto(sockfd, buffer, strlen(buffer), 0, (struct sockaddr*)&server, len);
         memset(buffer, 0, BUF_SIZE);
-        recvfrom(sockfd, buffer, BUF_SIZE, 0, (struct sockaddr*)&server, &len);
-        printf("Echo from server:%s",buffer);
+        recvfrom(sockfd, buffer, BUF_SIZE, 0, (struct sockaddr*)&client, &len);
+        printf("Received:%s",buffer);
+        sendto(sockfd, buffer, strlen(buffer), 0, (struct sockaddr*)&client, len);
     }
-
     close(sockfd);
     return 0;
 }
